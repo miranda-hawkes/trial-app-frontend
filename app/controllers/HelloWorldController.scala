df8e12ff027ef7c679f6f0ff7da4f8e8507e4e9e
Miranda Hawkes <miranda.hawkes@digital.hmrc.gov.uk>
2017-07-20 17:09:34 +0100

    Added first html partial

--- app/controllers/HelloWorldController.scala
+++ app/controllers/HelloWorldController.scala
@@ -2,18 +2,25 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
-import config.AppConfig
+import config.{AppConfig, WSHttp}
 import uk.gov.hmrc.play.frontend.controller.FrontendController
 import play.api.mvc._
 
 import scala.concurrent.Future
 import play.api.i18n.{I18nSupport, MessagesApi}
+import uk.gov.hmrc.play.http.HttpGet
+import uk.gov.hmrc.play.partials.{CachedStaticHtmlPartialRetriever, HtmlPartial}
 
 @Singleton
 class HelloWorldController @Inject()(appConfig: AppConfig,
-                                     val messagesApi: MessagesApi) extends FrontendController with I18nSupport {
+                                     val messagesApi: MessagesApi) extends FrontendController with I18nSupport with CachedStaticHtmlPartialRetriever {
+
+  override def httpGet: HttpGet = WSHttp
 
   val helloWorld: Action[AnyContent] = Action.async { implicit request =>
-    Future.successful(Ok(views.html.helloWorld(appConfig)))
+    Future.successful(Ok(views.html.helloWorld(appConfig, retrieveNIPartial.successfulContentOrEmpty)))
   }
+
+  def retrieveNIPartial(implicit request: Request[_]): HtmlPartial = loadPartial("http://localhost:9000/trial-app-frontend/national-insurance")
+
 }
